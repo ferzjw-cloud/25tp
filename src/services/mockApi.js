ff599416e74e09d39d46d386abe22d698f81e918
ferzjw <ferz.jw@gmail.com>
2026-02-20 16:31:20 -0300

    base del TP final

---
+++ src/services/mockApi.js
@@ -0,0 +1,9 @@
+const users = [
+  { name: "Cristian Villalba", status: "online" },
+  { name: "Lucas Audia", status: "online" },
+  { name: "Lis Lares", status: "online" },
+  { name: "Esteban Gomez", status: "offline" },
+  { name: "Ivan Lares", status: "online" },
+];
+
+export { users }
