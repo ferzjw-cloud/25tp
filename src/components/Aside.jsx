309097e935abecb6fd33da0de368e18f92b299ed
ferzjw <ferz.jw@gmail.com>
2026-02-23 11:11:52 -0300

    add: cambios en chat

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -4,11 +4,32 @@ import { users } from "../services/mockApi.js"
 const Aside = () => {
   const [search, setSearch] = useState("")
 
+  const fetchingData = async () => {
+    try {
+      const response = await fetch("https://dummyjson.com/users")
+
+      if (!response.ok) {
+        alert("No se encuentra")
+        return
+      }
+
+   const data = await response.json()
+
+      setUsers(data.users)
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  useEffect(() => {
+    fetchingData()
+  }, [])
+
   const handleChange = (event) => {
     setSearch(event.target.value)
   }
 
-  const filteredUsers = users.filter((user) => user.name.toLowerCase().includes(search.toLowerCase()))
+  const filteredUsers = users.filter((user) => user.firstName.toLowerCase().includes(search.toLowerCase()))
 
   return (
     <aside>
--- src/services/mockApi.js
+++ src/services/mockApi.js
@@ -1,9 +1,9 @@
 const users = [
-  { name: "Cristian Villalba", status: "online" },
-  { name: "Lucas Audia", status: "online" },
-  { name: "Lis Lares", status: "online" },
-  { name: "Esteban Gomez", status: "offline" },
-  { name: "Ivan Lares", status: "online" },
+  { id: 1, name: "Maite Reynoso", status: "online" },
+  { id: 2, name: "Axel Pobian", status: "online" },
+  { id: 3, name: "Anabella Vicentin", status: "online" },
+  { id: 4, name: "Nicolas Carabella", status: "offline" },
+  { id: 5, name: "Lucas Pensotti", status: "online" }
 ];
 
 export { users }
